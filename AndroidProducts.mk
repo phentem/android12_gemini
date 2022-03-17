ab29bc35d0126019a499aa5a13c751d66bbc2636
phentem <phentem@gmail.com>
2022-03-17 21:26:51 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_gemini.mk
+    $(LOCAL_DIR)/aosp_gemini.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_gemini-user \
-    evolution_gemini-userdebug \
-    evolution_gemini-eng
+    aosp_gemini-user \
+    aosp_gemini-userdebug \
+    aosp_gemini-eng
